ac469549a3a5c03e2460ac2e66a333c9f505670c
gpsign <gpsignorini@gmail.com>
2023-05-07 23:07:40 -0300

    User dinamico

--- src/User.js
+++ src/User.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
+
 export default function User() {
+  let [nome, setNome] = useState("catanacomics");
+  let [foto, setFoto] = useState("assets/img/catanacomics.svg");
   return (
     <>
       <div class="usuario">
-        <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+        <img
+          src={foto}
+          alt="imagem de perfil"
+          onClick={() => {
+            let entrada = prompt("URL da nova imagem:");
+            if(entrada)setFoto(entrada);
+          }}
+        />
         <div class="texto">
           <span>
-            <strong>catanacomics</strong>
-            <ion-icon name="pencil"></ion-icon>
+            <strong>{nome}</strong>
+            <ion-icon
+              name="pencil"
+              onClick={() => {
+                let entrada = prompt("Edite seu nome:");
+                if (entrada) setNome(entrada);
+              }}
+            ></ion-icon>
           </span>
         </div>
       </div>
